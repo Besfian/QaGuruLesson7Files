8e8e785551c4b259d6a0b3950a9959f79b27ec5b
besfian <besfian@mail.ru>
2021-11-17 03:42:52 +0300

    *

--- src/test/java/ru/mail/besfian/FileTests.java
+++ src/test/java/ru/mail/besfian/FileTests.java
@@ -14,41 +14,23 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FileTests {
-    //    @BeforeAll
-//    static void disclosure() {
-//        Configuration.startMaximized = true;
-//    }
-//    @Test
-//@Owner("ioomoon")
-//@DisplayName("Download PDF file and check it's content")
-//void downloadPDFFile() throws IOException {
-//    open(http://foxtools.ru/Base64);
-//
-//    File pdf = $(byText("Download sample pdf file")).download();
-//    PDF parsedPdf = new PDF(pdf);
-//    Assertions.assertEquals(4, parsedPdf.numberOfPages);
-//}
-//}
     @Test
-    @DisplayName("Работа с .txt файлом (скачивание и поиск по содержимому)")
-    void downloadTextFileAndSearchInsideIt() throws IOException {
+    @DisplayName("Работа с .txt файлом")
+    void txtFileDownload() throws IOException {
         open("https://github.com/Besfian/QaGuruLesson7Files/blob/main/build.gradle");
         File download = $("#raw-url").download();
         String fileContent = IOUtils.toString(new FileReader(download));
         assertTrue(fileContent.contains("com.codeborne:selenide:6.0.3"));
         sleep(5000);
-
     }
 
     @Test
-    @DisplayName("Загрузка файла по относительному пути (рекомендуется!)")
-    void filenameShouldDisplayedAfterUploadActionFromClasspathTest() {
+    @DisplayName("Загрузка файла по относительному пути")
+    void fileUpload() {
         open("https://tools.icoder.uz/file-to-base64-converter.php");
         $("#file").uploadFromClasspath("signature1.png");
         $(".form-control").shouldHave(text("data:image/png;base64"));
@@ -57,20 +39,11 @@ public class FileTests {
 
     @Test
     @DisplayName("Скачивание PDF файла")
-    void pdfFileDownloadTest() throws IOException {
-
-        step("Opening the repository Besfian", () -> {
-            open("https://github.com/Besfian/QaGuruLesson7Files/blob/main/src/test/resources/portationRequest9.pdf");
-        });
-        step("pdf Besfian", () -> {
-            File pdf = $("#raw-url").download();
-            PDF parsedPdf = new PDF(pdf);
-            Assertions.assertEquals(2, parsedPdf.numberOfPages);
-            Assertions.assertEquals("Сергеев",parsedPdf.keywords );
-//            assertTrue(parsedPdf. ("Сергеев");
-//            assertTrue(parsedPdf.keywords.contains("Сергеев"));
-
-        });
+    void pdfFileDownload() throws IOException {
+        open("https://github.com/Besfian/QaGuruLesson7Files/blob/main/src/test/resources/portationRequest10.pdf");
+        File pdf = $("#raw-url").download();
+        PDF parsedPdf = new PDF(pdf);
+        Assertions.assertEquals(2, parsedPdf.numberOfPages);
         sleep(5000);
     }
 }
